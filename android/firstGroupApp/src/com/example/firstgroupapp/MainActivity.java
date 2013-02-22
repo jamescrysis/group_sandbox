53fe71f384121dd6a6abad5d916c8807075ba40f
scottsnkwcz <scottsnkwcz@gmail.com>
2013-02-22 16:52:39 +1100

    First Commit on Group App - Added basic buttons to main activity - Created Map Activity - Added widgets to Map Activity - Added button listeners via implements method

--- android/firstGroupApp/src/com/example/firstgroupapp/MainActivity.java
+++ android/firstGroupApp/src/com/example/firstgroupapp/MainActivity.java
@@ -2,21 +2,61 @@ package com.example.firstgroupapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private Button mapSearch;
+	private Button exit;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+               
+        mapSearch = (Button) findViewById(R.id.button1);     
+        exit = (Button) findViewById(R.id.button2); 
+        
+        mapSearch.setOnClickListener(this);
+        exit.setOnClickListener(this);
+  
+        
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+ 
+    	
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	
+	public void onClick(View btn) {
+		
+		if(btn == mapSearch){			
+			
+			//Start Map Activity
+			Intent intent = new Intent(this, MapActivity.class);			
+			startActivity(intent);
+			
+			
+			
+		}
+		else if(btn == exit){
+			
+			// Close Program
+			finish();
+		}
+		
+		
+	}
     
 }
